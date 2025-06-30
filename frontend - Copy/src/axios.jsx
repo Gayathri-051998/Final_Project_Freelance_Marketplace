9c0c6574273619ba4e61657a363bee946bd7a1d1
Gayathri-051998 <gayathrivelayutham52@gmail.com>
2025-06-30 10:13:02 +0530

    WIP: staged changes before rebase

--- frontend - Copy/src/axios.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://final-project-freelance-marketplace.onrender.com' // backend URL
-});
-
-export default instance;
--- frontend - Copy/vite.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    server: {
-      proxy: {
-        '/api': 'https://final-project-freelance-marketplace.onrender.com',
-      },
-    },
-  };
-  
